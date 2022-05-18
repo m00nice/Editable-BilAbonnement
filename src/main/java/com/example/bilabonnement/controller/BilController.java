2a5383fce0b6a22ce843fdcabc67dbec781eedb4
cemakay <cema0092@stud.kea.dk>
2022-05-18 09:04:44 +0200

    metode i bilcontroller

--- src/main/java/com/example/bilabonnement/controller/BilController.java
+++ src/main/java/com/example/bilabonnement/controller/BilController.java
@@ -2,14 +2,19 @@ package com.example.bilabonnement.controller;
 
 import com.example.bilabonnement.models.Bil;
 import com.example.bilabonnement.repository.BilRepository;
+import com.example.bilabonnement.service.BilService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.bilabonnement.repository.IRepository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.ArrayList;
 
 @Controller
 public class BilController {
     private final IRepository<Bil> bilRepository = new BilRepository();
+    private final BilService bilService = new BilService();
 
     //TODO get method til at lave liste af biler med skade
     @GetMapping("/listeAfSkadetBiler")
@@ -20,5 +25,11 @@ public class BilController {
 
     //TODO get method til at lave liste af biler uden skade
 
+    @GetMapping("/getListeAfBilerUdenSkade")
+    public String alleBilerUdenSkade(@ModelAttribute(name= "skadeListe") Model model){
+        ArrayList<Bil> bilArrayList = bilService.getBilerUdenFejlOgMangler();
+        model.addAttribute("skadeListe", bilArrayList);
+        return "Skade&Udbedring";
+    }
 
 }
