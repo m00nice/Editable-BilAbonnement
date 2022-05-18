30f437ece10aca03d4a429171fff63a60562bf41
m00ni <m00nice@live.com>
2022-05-18 12:17:24 +0200

    bruh

--- src/main/java/com/example/bilabonnement/service/PersonaleService.java
+++ src/main/java/com/example/bilabonnement/service/PersonaleService.java
@@ -4,26 +4,25 @@ package com.example.bilabonnement.service;
 import com.example.bilabonnement.models.Personale;
 import com.example.bilabonnement.repository.PersonaleRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PersonaleService {
     private final PersonaleRepository personaleRepository = new PersonaleRepository();
 
     public boolean checkBruger(String brugernavn, String password) {
-        List<Personale> list = personaleRepository.getAlleBrugere();
-        while (list.iterator().hasNext()) {
-            if (list.iterator().next().getBrugernavn().equals(brugernavn) || list.iterator().next().getPassword().equals(password)) {
-                return true;
-            }
+        ArrayList<Personale> personalelist = (ArrayList<Personale>) personaleRepository.getAlleBrugere();
+        for (Personale personale: personalelist) {
+            if(personale.getBrugernavn().equals(brugernavn)&&personale.getPassword().equals(password)){return true;}
         }
         return false;
     }
 
     public String getRolle(String brugernavn, String password) {
-        List<Personale> list = personaleRepository.getAlleBrugere();
-        while (list.iterator().hasNext()) {
-            if (list.iterator().next().getBrugernavn().equals(brugernavn) || list.iterator().next().getPassword().equals(password)) {
-                return list.iterator().next().getRolle();
+        ArrayList<Personale> personalelist = (ArrayList<Personale>) personaleRepository.getAlleBrugere();
+        for (Personale personale: personalelist) {
+            if (personale.getBrugernavn().equals(brugernavn)&&personale.getPassword().equals(password)) {
+                return personale.getRolle();
             }
         }
         return "bruh";
