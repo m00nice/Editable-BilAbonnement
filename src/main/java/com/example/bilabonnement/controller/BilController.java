9e7564dfa092049973d96f14627b06c91f9d0300
m00ni <m00nice@live.com>
2022-05-19 09:21:48 +0200

    bruh

--- src/main/java/com/example/bilabonnement/controller/BilController.java
+++ src/main/java/com/example/bilabonnement/controller/BilController.java
@@ -15,7 +15,7 @@ public class BilController {
 
     //TODO get method til at lave liste af biler med skade
     @GetMapping("/SkadeOgUdebedring/Biler-med-skader")
-     public String alleSkadetBiler(@ModelAttribute (name = "skadeListe") Model model) {
+     public String alleSkadetBiler(Model model) {
       ArrayList<Bil> skadeArrayList = bilService.getBilerMedFejlOgMangler();
       model.addAttribute("skadeListe", skadeArrayList);
       return "SkadeOgUdbedring";
@@ -25,14 +25,14 @@ public class BilController {
     //TODO get method til at lave liste af biler uden skade
 
     @GetMapping("/SkadeOgUdbedring/Biler-uden-skader")
-    public String alleBilerUdenSkade(@ModelAttribute(name= "skadeListe") Model model) {
+    public String alleBilerUdenSkade(Model model) {
         ArrayList<Bil> bilArrayList = bilService.getBilerUdenFejlOgMangler();
         model.addAttribute("skadeListe", bilArrayList);
         return "SkadeOgUdbedring";
     }
 
     @GetMapping("/Forretningsudvikvikling")
-    public String listeAfUdlejedeBiler(@ModelAttribute(name = "billiste") Model model){
+    public String listeAfUdlejedeBiler(Model model){
         ArrayList<Bil> bilArrayList = bilService.getUdlejedeBiler();
         model.addAttribute("billiste",bilArrayList);
         return "Forretningudviklere";
